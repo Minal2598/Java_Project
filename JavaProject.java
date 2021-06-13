9e4c1854fa3895613e7036d041c19a53924eae6e
Minal2598 <84303020+Minal2598@users.noreply.github.com>
2021-06-13 15:38:00 +0530

    Update JavaProject.java

--- JavaProject.java
+++ JavaProject.java
@@ -2,7 +2,7 @@ package com.javaproject;
 
 public class JavaProject {
 	public static void main(String[] args) {
-		System.out.println("My first Java Program");
+		System.out.println("My first Java Program.....HI");
 		
 	}
 
